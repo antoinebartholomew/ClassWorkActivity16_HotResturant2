b18509c865ea718fabcbf5bec743e3cd1bea9e46
antoinebartholomew <antoinebartholomew@gmail.com>
2018-02-24 13:26:06 -0500

    another one 2

--- server.js
+++ server.js
@@ -3,58 +3,91 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
 // Reservation & waitlist data
 // =============================================================
 var tableRes = [
   {
-    name: "",
+    name: "Mary",
     phoneNumber: "",
     email: "",
-    id: ""
-  }
+    id: "",
+    status: "seated"
+  },
+    {
+    name: "John",
+    phoneNumber: "",
+    email: "",
+    id: "",
+    status: "waitlist"
+  },
 ];
+
 // Routes
 // =============================================================
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
+
 app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "views", "reserve.html"));
 });
+
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "views", "tables.html"));
 });
+
 // Get all reservations
-app.get("/all", function(req, res) {
+app.get("/api/all", function(req, res) {
   res.json(tableRes);
 });
+
 // Search for all reservations - provides JSON
-app.get("/api/:tableRes?", function(req, res) {
-  var chosen = req.params.tableRes;
+app.get("/api/:tables", function(req, res) {
+  var chosen = req.params.tables;
+  var matched = [];
+
   if (chosen) {
     console.log(chosen);
+
     for (var i = 0; i < tableRes.length; i++) {
-      if (chosen === tableRes[i].routeName) {
-        return res.json(tableRes[i]);
+      if (tableRes[i].status === chosen) {
+      	matched.push(tableRes[i]);
       }
     }
-    return res.json(false);
+    return res.json(matched);
   }
-  return res.json(tableRes);
+  return res.json(false);
 });
-// // Create New reservation - takes in JSON input
-// app.post("/api/new", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body-parser middleware
-//   var newReservation = req.body;
+
+// Create New reservation - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  tableRes.push(newReservation);
+
+  res.json(newReservation);
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
